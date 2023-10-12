0b22eb30a742582103286d2a5c9b9787c5dd79e8
Niklas Åsberg <nikas214@student.liu.se>
2023-10-12 09:58:19 +0200

    Logic layer: added interfaces

--- account-project/account-logic/src/main/java/se/liu/ida/tdp024/account/logic/api/facade/AccountLogicFacade.java
+++ account-project/account-logic/src/main/java/se/liu/ida/tdp024/account/logic/api/facade/AccountLogicFacade.java
@@ -1,6 +1,18 @@
 package se.liu.ida.tdp024.account.logic.api.facade;
 
+import java.util.List;
+
+import se.liu.ida.tdp024.account.data.api.entity.Account;
+import se.liu.ida.tdp024.account.data.api.entity.Transaction;
 
 public interface AccountLogicFacade {
-    
+    public String create(String accountType, String personKey, String bankName);
+
+    public List<Account> find(String personKey);
+
+    public String debit(String accountID, int amount);
+
+    public String credit(String accountID, int amount);
+
+    public List<Transaction> transactions(String accountID);
 }
--- account-project/account-logic/src/main/java/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
+++ account-project/account-logic/src/main/java/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
@@ -1,14 +1,57 @@
 package se.liu.ida.tdp024.account.logic.impl.facade;
 
+import java.util.List;
+
+import se.liu.ida.tdp024.account.data.api.entity.Account;
+import se.liu.ida.tdp024.account.data.api.entity.Transaction;
 import se.liu.ida.tdp024.account.data.api.facade.AccountEntityFacade;
+import se.liu.ida.tdp024.account.data.api.facade.TransactionEntityFacade;
 import se.liu.ida.tdp024.account.logic.api.facade.AccountLogicFacade;
+import se.liu.ida.tdp024.account.logic.api.service.BankAPI;
+import se.liu.ida.tdp024.account.logic.api.service.PersonAPI;
 
 public class AccountLogicFacadeImpl implements AccountLogicFacade {
     
     private AccountEntityFacade accountEntityFacade;
+    private TransactionEntityFacade transactionEntityFacade;
+    private BankAPI bankAPI;
+    private PersonAPI personAPI;
     
-    public AccountLogicFacadeImpl(AccountEntityFacade accountEntityFacade) {
+    public AccountLogicFacadeImpl(AccountEntityFacade accountEntityFacade, TransactionEntityFacade transactionEntityFacade, BankAPI bankAPI, PersonAPI personAPI) {
         this.accountEntityFacade = accountEntityFacade;
+        this.transactionEntityFacade = transactionEntityFacade;
+        this.bankAPI = bankAPI;
+        this.personAPI = personAPI;
+    }
+
+    @Override
+    public String create(String accountType, String personKey, String bankName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public List<Account> find(String personKey) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'find'");
+    }
+
+    @Override
+    public String debit(String accountID, int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'debit'");
+    }
+
+    @Override
+    public String credit(String accountID, int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'credit'");
+    }
+
+    @Override
+    public List<Transaction> transactions(String accountID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'transactions'");
     }
     
 }
